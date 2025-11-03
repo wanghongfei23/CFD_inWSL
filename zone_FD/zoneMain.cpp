38f10b1531cdb9dbdb0f8e115d3ee646473295a1
wanghongfei23 <whf00101@163.com>
2025-11-03 23:21:43 +0800

    v1.0.2-2主代码+2工具

--- zone_FD/zoneMain.cpp
+++ zone_FD/zoneMain.cpp
@@ -81,24 +81,24 @@ int main()
     // info->dim=1;
 
     // sod tube
-    // info->CFL = 0.5;
-    // info->endStep = 2;
-    // info->outputDt = 0.1;
-    // info->nCase = 0;
-    // info->calZone = { -0.5, 0.5, 0, 0, 0, 0 };
-    // info->iMax = { 101, 2, 2 };
-    // // info->iMax = { 1001, 2, 2 };
-    // info->dim = 1;
+    info->CFL = 0.5;
+    info->endStep = 2;
+    info->outputDt = 0.1;
+    info->nCase = 0;
+    info->calZone = { -0.5, 0.5, 0, 0, 0, 0 };
+    info->iMax = { 101, 2, 2 };
+    // info->iMax = { 1001, 2, 2 };
+    info->dim = 1;
 
     // lax sod tube
-    info->endStep=14;
-    info->outputDt=0.01;
-    info->CFL=0.5;
-    info->nCase=2;
-    info->calZone={-0.5,0.5,0,0,0,0};
-    info->iMax={201,2,2};
-    // info->iMax={2001,2,2};
-    info->dim=1;
+    // info->endStep=14;
+    // info->outputDt=0.01;
+    // info->CFL=0.5;
+    // info->nCase=2;
+    // info->calZone={-0.5,0.5,0,0,0,0};
+    // info->iMax={201,2,2};
+    // // info->iMax={2001,2,2};
+    // info->dim=1;
 
     // lax sod tube speed test
     // info->endStep=14;
