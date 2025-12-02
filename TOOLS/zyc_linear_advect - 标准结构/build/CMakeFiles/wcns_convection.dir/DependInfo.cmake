6cba5206fafec81599ac71dfa5a626b4ef0ad0b4
wanghongfei23 <whf00101@163.com>
2025-12-02 10:56:58 +0800

    v2.1.0-初步

---
+++ TOOLS/zyc_linear_advect - 标准结构/build/CMakeFiles/wcns_convection.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/archwanghongfei/Documents/GitHub/CFD_codes_tools/tools_CFD/zyc_linear_advect - 标准结构/src/with_time.cpp" "CMakeFiles/wcns_convection.dir/src/with_time.cpp.o" "gcc" "CMakeFiles/wcns_convection.dir/src/with_time.cpp.o.d"
+  "" "/home/archwanghongfei/Documents/GitHub/CFD_codes_tools/tools_CFD/zyc_linear_advect - 标准结构/bin/wcns_convection" "gcc" "CMakeFiles/wcns_convection.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ TOOLS/zyc_linear_advect - 标准结构/include/000_globals.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <vector>
+#include <map>
+#include <string>
+
+// 注意：使用 inline 关键字确保在多个编译单元中只有一个实例
+
+inline std::string global_string ;
+
